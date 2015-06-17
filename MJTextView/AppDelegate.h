dfa190d2a23b072c251264ffac0923fa82540cef
yangbin <18774625353@139.com>
2015-06-17 16:29:50 +0800

    Initial Commit

---
+++ MJTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJTextView
+//
+//  Created by 杨彬 on 15/6/17.
+//  Copyright (c) 2015年 macbook air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJTextView
+//
+//  Created by 杨彬 on 15/6/17.
+//  Copyright (c) 2015年 macbook air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
